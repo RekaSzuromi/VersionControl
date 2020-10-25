0a3c4c77e3d6b792a66f3a6e86ea0e8619dd6292
Réka Szuromi <szuromir2@gmail.com>
2020-10-25 11:12:51 +0100

    3. feladat

---
+++ week07/week07/Entities/Gender.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week07.Entities
+{
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2
+    }
+
+    // Példa enumerációs változók használatára
+    Gender ádámNeme = Gender.Male;
+    Gender évaNeme = (Gender)2;
+    
+}
