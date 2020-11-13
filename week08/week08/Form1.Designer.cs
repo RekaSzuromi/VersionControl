42957c781b4b896c3095176793bc17c4f6e9569d
Réka Szuromi <szuromir2@gmail.com>
2020-11-13 17:43:35 +0100

    11 half

--- week08/week08/Form1.Designer.cs
+++ week08/week08/Form1.Designer.cs
@@ -32,11 +32,18 @@
             this.mainPanel = new System.Windows.Forms.Panel();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.mainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainPanel
             // 
             this.mainPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.mainPanel.Controls.Add(this.button2);
+            this.mainPanel.Controls.Add(this.button1);
+            this.mainPanel.Controls.Add(this.label1);
             this.mainPanel.Location = new System.Drawing.Point(4, 3);
             this.mainPanel.Name = "mainPanel";
             this.mainPanel.Size = new System.Drawing.Size(764, 445);
@@ -52,6 +59,33 @@
             this.conveyorTimer.Enabled = true;
             this.conveyorTimer.Interval = 10;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Coming next";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(132, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Car";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(299, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Ball";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -61,6 +95,8 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -70,6 +106,9 @@
         private System.Windows.Forms.Panel mainPanel;
         private System.Windows.Forms.Timer createTimer;
         private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- week08/week08/Form1.cs
+++ week08/week08/Form1.cs
@@ -58,5 +58,7 @@ namespace week08
         {
 
         }
+
+
     }
 }
